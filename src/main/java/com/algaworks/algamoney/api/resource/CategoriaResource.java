897e35d968c035e2ffd8965df866aa3a781e0cae
cleberGraciano <cleber_batista2010@hotmail.com>
2019-05-15 01:02:17 -0300

    Criando Api

---
+++ src/main/java/com/algaworks/algamoney/api/resource/CategoriaResource.java
@@ -0,0 +1,42 @@
+package com.algaworks.algamoney.api.resource;
+
+import com.algaworks.algamoney.api.model.Categoria;
+import com.algaworks.algamoney.api.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @GetMapping
+    public  List<Categoria> listar(){
+
+        return  categoriaRepository.findAll();
+        // -Metodo errado pois fica confuso return !categorias.isEmpty() ? ResponseEntity.ok(categorias) : ResponseEntity.noContent().build();
+
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void criar(@RequestBody Categoria categoria, HttpServletResponse response){
+       Categoria categoriaSalva =  categoriaRepository.save(categoria);
+       //Atraves da Classe ServletUriComponentsBuilder eu vou pegar a partir da requisição atual que foi /categorias adicionar o codigo
+        //e adicionar esse codigo na Uri, setar o header com está uri
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+                .buildAndExpand(categoriaSalva.getCodigo()).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+    }
+
+}
